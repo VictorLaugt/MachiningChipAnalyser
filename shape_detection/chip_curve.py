b1e348684cd1d96a9519f1e0da5f03adbad0e522
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-06-05 20:10:35 +0300

    Start to implement chip curve detection

--- shape_detection/chip_curve.py
+++ shape_detection/chip_curve.py
@@ -4,13 +4,52 @@ import numpy as np
 import cv2 as cv
 
 
+def above_line(points, a, b, c, min_distance):
+    """Filter the points to keep those which verify a*x + b*y + c >= min_distance"""
+    x, y = points[..., 0], points[..., 1]
+    return points[a*x + b*y + c >= min_distance]
+
+
+def filter_curve(hull_points, maximum_angle, lateral_margin):
+    """Filter the points of hull to only keep the chip curve"""
+    if len(hull_points) < 3:
+        return hull_points[:]
+
+    points = hull_points.reshape(-1, 2)
+    for i in range(2, len(points)):
+        a, b, c = points[i-2], points[i-1], points[i]
+
+        if c[0] < lateral_margin:  # image lateral border reached
+            return hull_points[:i]
+
+        ab, bc = (b - a), (c - b)
+        angle = np.arccos(np.dot(ab, bc) / (np.linalg.norm(ab) * np.linalg.norm(bc)))
+        if angle > maximum_angle:  # sharp angle detected (i.e end of chip reached)
+            return hull_points[:i]
+
+    return hull_points[:]
+
+
 def extract_chip_curve(binary):
     extracted_shape = binary.copy()
     contours, _hierarchy = cv.findContours(binary[:, :967], cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
     points = np.vstack(contours)  # ~ (n, 1, 2)
-    hull = cv.convexHull(points)  # ~ (p, 1, 2)
 
-    cv.drawContours(extracted_shape, (hull,), 0, 127, 0)
+    # MOCK: remove the tool and the base
+    points = above_line(points, a=0, b=-1, c=385, min_distance=5)  # above the base
+    points = above_line(points, a=-1, b=0, c=967, min_distance=5)  # at the left of the tool
+    origin = np.array([[967, 385]], dtype=points.dtype)
+
+    hull_points = cv.convexHull(points, clockwise=True)  # ~ (p, 1, 2)
+    start_index = np.argmin(np.linalg.norm(hull_points - origin, axis=-1))
+    hull_points = np.roll(hull_points, -start_index, axis=0)
+
+    curve_points = filter_curve(hull_points, maximum_angle=np.pi/4, lateral_margin=20)
+
+    cv.polylines(extracted_shape, (curve_points,), isClosed=False, color=127, thickness=0)
+
+
+    # cv.drawContours(extracted_shape, (hull_points,), 0, 127, 0)
 
     return extracted_shape
 
