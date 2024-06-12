5084913b1405f7ce7f83a27afa517a1248d179df
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-06-12 11:01:17 +0300

    Fix a typo in the convex hull point filter

--- shape_detection/constrained_hull_polynomial.py
+++ shape_detection/constrained_hull_polynomial.py
@@ -32,22 +32,23 @@ def extract_chip_curve(precise, rough):
 
     # compute the convex hull and constrain it to cross an anchor point
     y_min = points[points[:, :, 1].argmin(), 0, 1]
-    x_min = points[points[:, :, 0].argmin(), 0, 0]
-    anchor = np.array([[[0, y_min]], [[x_min, h-1]]])
+    # x_min = points[points[:, :, 0].argmin(), 0, 0]
+    anchor = np.array([[[0, y_min]], [[0, h-1]]])
     hull_points = cv.convexHull(np.vstack((points, anchor)))
     first_point_index = np.where(
         (hull_points[:, 0, 0] == anchor[0, 0, 0]) &
         (hull_points[:, 0, 1] == anchor[0, 0, 1])
     )[0][0]
     hull_points = np.roll(hull_points, -first_point_index, axis=0)
-    hull_points = hull_points[hull_points[:, 0, 1] > 15]
 
-    # remove from the convex hull points near the tool and the base
-    chip_curve_keys = filter_between_base_tool(hull_points, base_line, tool_line, 20, 20)
+    # remove from the convex hull points near the tool, the base, and the border of the image
+    key_points = hull_points[2:]
+    key_points = key_points[key_points[:, 0, 1] > 15]
+    key_points = filter_between_base_tool(key_points, base_line, tool_line, 20, 20)
 
     # extract points of the chip curve
     chip_curve_mask = np.zeros((h, w), dtype=np.uint8)
-    draw_chip_curve(chip_curve_mask, chip_curve_keys)
+    draw_chip_curve(chip_curve_mask, key_points)
     y, x = np.nonzero(rough & chip_curve_mask)
     chip_curve_points = np.stack((x, y), axis=1).reshape(-1, 1, 2)
 
@@ -57,7 +58,7 @@ def extract_chip_curve(precise, rough):
     else:
         parabola = Polynomial.fit(chip_curve_points[:, 0, 0], chip_curve_points[:, 0, 1], 2)
 
-    return parabola, hull_points, base_line, tool_line
+    return parabola, hull_points, key_points, base_line, tool_line
 
 
 def render_chip_curve(precise, rough, render=None):
@@ -68,7 +69,7 @@ def render_chip_curve(precise, rough, render=None):
     else:
         render = render.copy()
 
-    parabola, key_points, base_line, tool_line = extract_chip_curve(precise, rough)
+    parabola, hull_points, key_points, base_line, tool_line = extract_chip_curve(precise, rough)
     if parabola is not None:
         x = np.arange(0, w, 1, dtype=np.int32)
         y = parabola(x).astype(np.int32)
@@ -79,8 +80,10 @@ def render_chip_curve(precise, rough, render=None):
     geometry.draw_line(render, *base_line, color=127, thickness=1)
     geometry.draw_line(render, *tool_line, color=127, thickness=1)
 
+    for pt in hull_points.reshape(-1, 2):
+        cv.circle(render, pt, 10, 127 // 2, -1)
     for kpt in key_points.reshape(-1, 2):
-        cv.circle(render, kpt, 5, 127, -1)
+        cv.circle(render, kpt, 10, 127, -1)
 
     return render
 
@@ -101,7 +104,7 @@ if __name__ == '__main__':
 
     processing.run(loader, output_dir)
     processing.compare_frames(15, ("morph", "chipcurve"))
-    processing.compare_videos(("input", "chipcurve"))
+    processing.compare_videos(("input", "chipcurve"), horizontal=False)
 
 
 # if __name__ == '__main__':
