1deb6ffa4c8244b3fad0d6c5e137a040c2e837fc
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-06-17 13:09:29 +0300

    Optimize the `remove_small_components` blob filter function

--- connected_components.py
+++ connected_components.py
@@ -3,24 +3,26 @@ import numpy as np
 
 
 def remove_small_components(img, min_area=45):
-    analysis = cv.connectedComponentsWithStats(img, 8, cv.CV_32S)
-    total_labels, label_ids, values, centroids = analysis
+    num_labels, labels, stats, _ = cv.connectedComponentsWithStats(img, connectivity=8)
 
     filtered_img = np.zeros_like(img)
+    keep_mask = np.zeros_like(labels, dtype=bool)
 
-    for i in range(1, total_labels):
-        if values[i, cv.CC_STAT_AREA] >= min_area:
-            component = np.where(label_ids == i, 255, 0).astype(np.uint8)
-            filtered_img |= component
+    for i in range(1, num_labels):
+        if stats[i, cv.CC_STAT_AREA] >= min_area:
+            keep_mask |= (labels == i)
+
+    filtered_img[keep_mask] = 255
 
     return filtered_img
 
 
 if __name__ == "__main__":
     img = cv.imread("morph2.png", cv.IMREAD_GRAYSCALE)
-    filtered = remove_small_components(img, min_area=45)
+    filtered_img = remove_small_components(img)
+
     cv.imshow("original", img)
-    cv.imshow("filtered", filtered)
+    cv.imshow("filtered", filtered_img)
     while cv.waitKey(30) != 113:
         pass
     cv.destroyAllWindows()
