207923b82a9da91fa3ffa7c333fc999263fdff39
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-06-19 17:37:21 +0300

    real time camera experiment

--- experiments/real_time_camera.py
+++ experiments/real_time_camera.py
@@ -1,10 +1,14 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    import numpy as np
+    from typing import Callable, TypeVar
+    Image = TypeVar("Image", bound=np.ndarray)
+
 import numpy as np
 import cv2 as cv
 
 import connected_components
-import video
-
-from pathlib import Path
 
 
 log_kernel = 2 * np.array([
@@ -20,45 +24,31 @@ log_kernel = 2 * np.array([
 ])
 
 
-
-cam = cv.VideoCapture(0)
-# cam = cv.VideoCapture(str(Path("experiments", "input.avi")))
-
-
-
-# Real-time image processing loop
-processed = []
-while True:
-    status, img = cam.read()
-    if not status:
-        break
-
+def processing(img: Image) -> Image:
     gray_img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
     edge_img = cv.filter2D(gray_img, -1, log_kernel)
     binary_img = cv.threshold(edge_img, 240, 255, cv.THRESH_BINARY)[1]
     blobfilter_img = connected_components.remove_small_components(binary_img, min_area=20)
     morph_img = cv.erode(blobfilter_img, cv.getStructuringElement(cv.MORPH_CROSS, (3, 3)), iterations=2)
-
-    processed.append(morph_img)
-    if len(processed) == 120:
-        break
-
-    # # Check for the 'Enter' key press to exit the loop
-    # if cv.waitKey(10) == 13:
-    #     break
+    return morph_img
 
 
-print("Displaying the processed images...")
-video.VideoImgSeqPlayer(processed, "player").play()
-cam.release()
-cv.destroyAllWindows()
+def real_time_processing(stream: cv.VideoCapture, processing: Callable[[Image], Image]) -> None:
+    while True:
+        status, img = stream.read()
+        if not status: # reached end of file
+            break
+        cv.imshow("processed image", processing(img))
+        if cv.waitKey(1) == 113: # user exits by pressing 'q'
+            break
+
+    cv.destroyAllWindows()
 
 
+if __name__ == '__main__':
+    from pathlib import Path
 
-# for img in processed:
-#     cv.imshow("processed", img)
-#     if cv.waitKey(10) == 120:
-#         break
-
-# cam.release()
-# cv.destroyAllWindows()
+    cam = cv.VideoCapture(0)
+    # cam = cv.VideoCapture(str(Path("experiments", "input.avi")))
+    real_time_processing(cam, processing)
+    cam.release()
