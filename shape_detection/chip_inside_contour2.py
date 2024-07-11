519464db6ac0e9d664a3b5b6c0de0c5dc89d35ab
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-07-11 17:05:31 +0300

    Make the raycasting algorithm robust to the chip rotation direction

--- shape_detection/chip_inside_contour2.py
+++ shape_detection/chip_inside_contour2.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
 import geometry
 from shape_detection.constrained_hull_polynomial import (
     compute_chip_convex_hull,
-    extract_chip_curve_points
+    # extract_chip_curve_points
 )
 from shape_detection.chip_extraction import extract_main_features
 
@@ -22,18 +22,29 @@ import skimage as ski
 
 
 @dataclass
-class ChipInsideFeatures:
+class InsideFeatures:
     thickness: Sequence[float]
     inside_contour_pts: Sequence[Point]
+
+
+def extract_chip_curve_points(main_ft: MainFeatures, chip_hull_pts: PointArray) -> PointArray:
+    """Return the points of the chip hull which belong to the chip curve."""
+    return geometry.under_lines(
+        chip_hull_pts,
+        (main_ft.base_line, main_ft.base_opp_border, main_ft.tool_opp_border),
+        (0, 15, 15)
+    )
 
 
 def direct_rotated_90(p: Point) -> Point:
     x, y = p
     return (-y, x)
 
+
 def indirect_rotated_90(p: Point) -> Point:
     x, y = p
     return (y, -x)
+
 
 def normalized(vector: Point) -> Point:
     x, y = vector
@@ -50,15 +61,17 @@ def rasterized_line(p0: Point, p1: Point, img_height: int, img_width: int) -> tu
 def find_inside_contour(
     binary_img: np.ndarray,
     chip_curve_pts: PointArray,
+    indirect_rotation: bool,
     thickness_majorant: int
-        ) -> ChipInsideFeatures:
+        ) -> InsideFeatures:
     h, w = binary_img.shape
+    rotated_90 = indirect_rotated_90 if indirect_rotation else direct_rotated_90
     thickness = []
     inside_contour_pts = []
 
     for i in range(len(chip_curve_pts)-1):
         a, b = chip_curve_pts[i, 0, :], chip_curve_pts[i+1, 0, :]
-        n = np.asarray(normalized(indirect_rotated_90(b - a)))
+        n = np.asarray(normalized(rotated_90(b - a)))
         c, d = (a + thickness_majorant*n).astype(np.int32), (b + thickness_majorant*n).astype(np.int32)
 
         for p0, p1 in zip(zip(*rasterized_line(a, b, h, w)), zip(*rasterized_line(c, d, h, w))):
@@ -74,10 +87,10 @@ def find_inside_contour(
                 thickness.append(distances[furthest_idx])
                 inside_contour_pts.append((selected_x[furthest_idx], selected_y[furthest_idx]))
 
-    return ChipInsideFeatures(thickness, inside_contour_pts)
+    return InsideFeatures(thickness, inside_contour_pts)
 
 
-def extract_chip_inside_contour(binary_img: np.ndarray) -> tuple[MainFeatures, ChipInsideFeatures]:
+def extract_chip_inside_contour(binary_img: np.ndarray) -> tuple[MainFeatures, InsideFeatures]:
     main_ft = extract_main_features(binary_img)
 
     contours, _ = cv.findContours(binary_img, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
@@ -87,27 +100,47 @@ def extract_chip_inside_contour(binary_img: np.ndarray) -> tuple[MainFeatures, C
     chip_hull_pts = compute_chip_convex_hull(main_ft, chip_pts)
     chip_curve_pts = extract_chip_curve_points(main_ft, chip_hull_pts)
 
-    inside_ft = find_inside_contour(binary_img, chip_curve_pts, thickness_majorant=125)
+    inside_ft = find_inside_contour(
+        binary_img,
+        chip_curve_pts,
+        main_ft.indirect_rotation,
+        thickness_majorant=125
+    )
 
     return main_ft, inside_ft
 
 
-def render_inside_features(render: np.ndarray, main_ft: MainFeatures, inside_ft: ChipInsideFeatures) -> None:
+def render_inside_features(render: np.ndarray, main_ft: MainFeatures, inside_ft: InsideFeatures) -> None:
     """Draw a representation of features `main_ft` and `inside_ft` on image `render`."""
     for x, y in inside_ft.inside_contour_pts:
         render[y, x] = (0, 0, 255)  # red
 
 
-def extract_and_render(binary_img: np.ndarray, background: np.ndarray|None=None) -> np.ndarray:
-    main_ft, inside_ft = extract_chip_inside_contour(binary_img)
-    if background is None:
-        ft_repr = np.zeros((binary_img.shape[0], binary_img.shape[1], 3), dtype=np.uint8)
-        render_inside_features(ft_repr, main_ft, inside_ft)
-        # ft_repr[np.nonzero(binary_img)] = (255, 255, 255)
-    else:
-        ft_repr = background.copy()
-        render_inside_features(ft_repr, main_ft, inside_ft)
-    return ft_repr
+class InsideFeatureCollector:
+    def __init__(self, scale: float=1.0):
+        self.scale = scale
+        self.inside_features: list[InsideFeatures] = []
+        self.main_features: list[MainFeatures] = []
+        self.chip_thickness_sequences: list[Sequence[float]] = []
+
+    def collect(self, main_ft: MainFeatures, inside_ft: InsideFeatures) -> None:
+        self.main_features.append(main_ft)
+        self.inside_features.append(inside_ft)
+        self.chip_thickness_sequences.append(
+            [self.scale * t for t in inside_ft.thickness]
+        )
+
+    def extract_and_render(self, binary_img: np.ndarray, background: np.ndarray|None=None) -> np.ndarray:
+        main_ft, inside_ft = extract_chip_inside_contour(binary_img)
+        self.collect(main_ft, inside_ft)
+        if background is None:
+            ft_repr = np.zeros((binary_img.shape[0], binary_img.shape[1], 3), dtype=np.uint8)
+            render_inside_features(ft_repr, main_ft, inside_ft)
+            # ft_repr[np.nonzero(binary_img)] = (255, 255, 255)
+        else:
+            ft_repr = background.copy()
+            render_inside_features(ft_repr, main_ft, inside_ft)
+        return ft_repr
 
 
 if __name__ == '__main__':
@@ -121,6 +154,7 @@ if __name__ == '__main__':
     # ---- environment variables
     input_dir_str = os.environ.get("INPUT_DIR")
     output_dir_str = os.environ.get("OUTPUT_DIR")
+    scale_str = os.environ.get("SCALE_UM")
 
     if input_dir_str is not None:
         input_dir = Path(input_dir_str)
@@ -132,11 +166,17 @@ if __name__ == '__main__':
     else:
         output_dir = Path("results", "chipvurve")
 
+    if scale_str is not None:
+        scale_um = float(scale_str)
+    else:
+        scale_um = 3.5
+
 
     # ---- processing
+    collector = InsideFeatureCollector(scale_um)
     processing = preprocessing.log_tresh_blobfilter_erode.processing.copy()
 
-    processing.add("chipinside", extract_and_render)
+    processing.add("chipinside", collector.extract_and_render)
 
     loader = image_loader.ImageLoader(input_dir)
     processing.run(loader)
@@ -145,3 +185,5 @@ if __name__ == '__main__':
     # ---- visualization
     processing.show_frame_comp(min(15, len(loader)-1), ("chipinside", "morph"))
     processing.show_video_comp(("chipinside", "morph"))
+
+
