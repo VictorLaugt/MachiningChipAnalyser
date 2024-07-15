73fc4a46a3d67342a4f87ec2be692beb273d63d9
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-07-15 15:42:25 +0300

    Continue testing the bisector algorithm

--- tests/bisectors.py
+++ tests/bisectors.py
@@ -1,78 +1,84 @@
+import unittest
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from shape_detection.chip_inside_contour3 import compute_bisectors
 
 
-def _angle_between_vectors(u, v):
+def angle_between_vectors(u, v):
     dot_products = np.sum(u*v, axis=1)
     norm_products = np.linalg.norm(u, axis=1) * np.linalg.norm(v, axis=1)
     return np.arccos(dot_products / norm_products)
 
 
-def _assert_middle_angles(pts, bisectors):
+def check_middle_angles_bisection(pts, bisectors):
     u = pts[:-2] - pts[1:-1]
     v = pts[2:] - pts[1:-1]
 
-    half_angles = _angle_between_vectors(u, v) / 2
-    ub_angles = _angle_between_vectors(u, bisectors[1:-1])
-    vb_angles = _angle_between_vectors(v, bisectors[1:-1])
+    ub_angles = angle_between_vectors(u, bisectors[1:-1])
+    vb_angles = angle_between_vectors(v, bisectors[1:-1])
 
-    assert np.isclose(ub_angles, half_angles).all()
-    assert np.isclose(vb_angles, half_angles).all()
+    return np.isclose(ub_angles, vb_angles).all()
 
 
-def _assert_bound_angles(pts, bisectors):
+def check_bound_angles_are_90(pts, bisectors):
     first_bisector, last_bisector = bisectors[0], bisectors[-1]
-    first_edge, last_edge = pts[1]-pts[0], pts[-1]-pts[-2]
+    first_edge, last_edge = (pts[1] - pts[0]), (pts[-1] - pts[-2])
 
     first_bound_angle = np.arccos(np.dot(first_bisector, first_edge) / np.linalg.norm(first_edge))
-    last_bound_angle = np.arccos(np.dot(last_bisector, last_edge)) / np.linalg.norm(last_edge)
+    last_bound_angle = np.arccos(np.dot(last_bisector, last_edge) / np.linalg.norm(last_edge))
 
-    print(f"{first_bound_angle=}")
-    print(f"{last_bound_angle=}")
-
-    assert np.isclose(first_bound_angle, np.pi/2)
-    assert np.isclose(last_bound_angle, np.pi/2)
-
-
-def _assert_unit_vectors(vectors):
-    assert np.isclose(np.linalg.norm(vectors, axis=1), 1.0).all()
+    return (
+        np.isclose(first_bound_angle, np.pi/2) and
+        np.isclose(last_bound_angle, np.pi/2)
+    )
 
 
-def _test_on_points(pts, direct_rotation=True):
-    bisectors = compute_bisectors(pts.reshape(-1, 1, 2), not direct_rotation)
-    _fig, ax = plt.subplots()
-    ax.set_aspect('equal', adjustable='box')
-    ax.grid()
-    ax.plot(pts[:, 0], pts[:, 1], '-x')
-    ax.quiver(pts[:, 0], pts[:, 1], bisectors[:, 0], bisectors[:, 1])
-    plt.show()
-
-    _assert_unit_vectors(bisectors)
-    _assert_middle_angles(pts, bisectors)
-    _assert_bound_angles(pts, bisectors)
+def are_unitary(vectors):
+    return np.isclose(np.linalg.norm(vectors, axis=1), 1.0).all()
 
 
-def test_triangle():
-    pts = np.array([[.0, .0], [.5, .5], [.0, .7]])
-    _test_on_points(pts)
+class TestBisectorAlgorithm(unittest.TestCase):
+    def _test_on_points(self, pts, direct_rotation=True, interactive=False):
+        bisectors = compute_bisectors(pts.reshape(-1, 1, 2), not direct_rotation)
+
+        if interactive:
+            _fig, ax = plt.subplots()
+            ax.set_aspect('equal', adjustable='box')
+            ax.grid()
+            ax.plot(pts[:, 0], pts[:, 1], '-x')
+            ax.quiver(pts[:, 0], pts[:, 1], bisectors[:, 0], bisectors[:, 1])
+            plt.show()
+
+        self.assertTrue(are_unitary(bisectors))
+        self.assertTrue(check_middle_angles_bisection(pts, bisectors))
+        self.assertTrue(check_bound_angles_are_90(pts, bisectors))
 
 
-def test__curve_evenly_spaced():
-    theta = np.linspace(0, 2*np.pi/3, 10)
-    pts = np.column_stack((np.cos(theta), np.sin(theta)))
-    _test_on_points(pts)
+    def test_triangle(self):
+        pts = np.array([[0., 0.], [1., 1.], [2., 0.]])
+        self._test_on_points(pts, direct_rotation=True)
+
+    def test_triangle2(self):
+        pts = np.array([[0., 0.], [1., 1.], [2., 0.]])
+        self._test_on_points(pts, direct_rotation=False, interactive=True)
+
+    def test_curve_evenly_spaced(self):
+        theta = np.linspace(0, 2*np.pi/3, 10)
+        pts = np.column_stack((np.cos(theta), np.sin(theta)))
+        self._test_on_points(pts, direct_rotation=True)
+
+    def test_curve_randomly_spaced(self):
+        theta = np.random.rand(10)
+        theta.sort()
+        theta *= 2*np.pi/3
+        pts = np.column_stack((np.cos(theta), np.sin(theta)))
+        self._test_on_points(pts, direct_rotation=True)
 
 
-def test__curve_randomly_spaced():
-    theta = np.random.rand(10)
-    theta.sort()
-    theta *= 2*np.pi/3
-    pts = np.column_stack((np.cos(theta), np.sin(theta)))
-    _test_on_points(pts)
+    # def test_random_points(self):
+    #     ...
 
 if __name__ == "__main__":
-    test_triangle()
-    # test__curve_evenly_spaced()
-    # test__curve_randomly_spaced()
+    unittest.main()
