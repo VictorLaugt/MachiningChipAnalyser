60d9fb98c3b38e15be739513abfe0785748796d2
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-08-09 13:12:51 +0300

    Code line count

--- importation-graph.sh
+++ importation-graph.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 PROJECT_DIR=$(dirname $(realpath $0))
 
-which inclusionmap
+printf "===== line count =====\n"
+find . -name '*.py' ! -path '*/tests/*' ! -path '*/__pycache__/*' | xargs wc -l
+
+printf "\n===== importation graph =====\n"
+which inclusionmap > /dev/null
 if [ $? == 0 ]; then
     inclusionmap ${PROJECT_DIR}/ChipAnalyser -l python --display-algorithm dot -s -i tests
 else
