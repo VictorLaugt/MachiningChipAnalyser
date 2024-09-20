5a75463e4059caaa2a29155a120bbb2deafff048
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-09-20 17:30:48 +0200

    Highlight test files in the importation graph

--- importation_graph.sh
+++ importation_graph.sh
@@ -7,7 +7,7 @@ find . -name '*.py' ! -path '*/tests/*' ! -path '*/__pycache__/*' | xargs wc -l
 printf "\n===== importation graph =====\n"
 which inclusionmap > /dev/null
 if [ $? == 0 ]; then
-    inclusionmap ${PROJECT_DIR}/ChipAnalyser -l python --display-algorithm dot -s -i tests
+    inclusionmap ${PROJECT_DIR}/ChipAnalyser -s -l python -g '.*test.*' --display-algorithm dot
 else
     echo "Should install InclusionMap to generate the graph: pip install InclusionMap"
 fi
