cdaf5fe1b6db592bab1c9f5f343280f8886bb324
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-09-30 23:34:08 +0200

    Improve calibration plots

--- ManualAnnotations/compare_measures.py
+++ ManualAnnotations/compare_measures.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import csv
 import numpy as np
 from numpy.polynomial import Polynomial
+
 import matplotlib.pyplot as plt
 
 
@@ -43,9 +44,13 @@ def etalonnage(measure, real, name):
     error = real - corrected
     expected_error = np.mean(error)
     standard_deviation = np.sqrt(np.mean((error - expected_error) ** 2))
-    uncertainty_80 = 1.28 * standard_deviation
-    uncertainty_90 = 1.65 * standard_deviation
-    uncertainty_95 = 1.96 * standard_deviation
+    uncertainty = {
+        80: 1.28 * standard_deviation,
+        85: 1.44 * standard_deviation,
+        90: 1.645 * standard_deviation,
+        95: 1.96 * standard_deviation,
+        99: 2.575 * standard_deviation,
+    }
 
     print(f"Mesure de la grandeur \"{name}\"")
     print(f"droite d'étalonnage: a = {a}, b = {b}")
@@ -53,40 +58,50 @@ def etalonnage(measure, real, name):
         "différence entre les mesures corrigées et les valeurs réelles: "
         f"espérance = {expected_error}, écart-type = {standard_deviation}"
     )
-    print(f"incertitude à un niveau de confiance de 80% = {uncertainty_80}")
-    print(f"incertitude à un niveau de confiance de 90% = {uncertainty_90}")
-    print(f"incertitude à un niveau de confiance de 95% = {uncertainty_95}")
+    for conf, uncert in uncertainty.items():
+        print(f"incertitude à un niveau de confiance de {conf}% = {uncert}")
     print()
 
-    fig, ax = plt.subplots(figsize=(10, 10))
-    ax.set_title(f"étalonnage: {name}")
+    fig, ax = plt.subplots(1, 2, figsize=(16, 8))
+    fig.suptitle(name)
 
     calibration_x = np.array((real.min(), real.max()))
     calibration_y = calibration(calibration_x)
-    ax.plot(real, measure, 'b+', label="mesures non corrigéesc")
-    ax.plot(calibration_x, calibration_y, 'b-')
+    ax[0].set_title("étalonnage")
+    ax[0].plot(real, measure, 'b+', label=f"mesures non corrigées, dispersées autour de y = {a:.3f}x + {b:.3f}")
+    ax[0].plot(real, corrected, 'r+', label=f"mesures corrigées, dispersées autour de y = x")
+    ax[0].plot(calibration_x, calibration_y, 'b-')
+    ax[0].plot(calibration_x, calibration_x, 'r-')
+    ax[0].set_xlabel("mesure manuelle")
+    ax[0].set_ylabel("mesure automatique")
+    ax[0].legend()
+    ax[0].grid()
 
-    ax.plot(real, corrected, 'r+', label="mesures corrigées")
-    ax.plot(calibration_x, calibration_x, 'r-')
-
-    ax.set_xlabel("mesure manuelle")
-    ax.set_ylabel("mesure automatique")
-    ax.legend()
-    ax.grid()
-
-    fig, ax = plt.subplots()
-    ax.set_title(f"distributions des erreurs de mesure: {name}")
+    ax[1].set_title("distribution des erreurs de mesure")
     x = np.linspace(error.min(), error.max(), 1000)
-    ax.hist(
+    ax[1].hist(
         error,
         bins=15,
         density=True,
         align='mid',
+        color='red',
+        alpha=0.3
         # width=0.7*(x[-1]-x[0])/(15)
     )
-    ax.plot(x, 1/(standard_deviation*np.sqrt(2*np.pi)) * np.exp(-1/2 * ((x - expected_error)/standard_deviation)**2))
+    ax[1].plot(
+        x, gaussian(x, mu=expected_error, sigma=standard_deviation), 'r-',
+        label=f"densité de la loi normale centrée d'écart-type {standard_deviation:.3f}",
+    )
+    ax[1].set_xlabel("différence entre la mesure automatique corrigée et la mesure manuelle")
+    ax[1].set_ylabel("densité de probabilité")
+    ax[1].legend()
+    ax[1].grid()
 
     plt.show()
+
+
+def gaussian(x, mu, sigma):
+    return np.exp(-0.5 * ((x - mu) / sigma)**2) / (sigma * np.sqrt(2*np.pi))
 
 
 if __name__ == '__main__':
@@ -108,17 +123,19 @@ if __name__ == '__main__':
 
     # Affiche les valeurs mesurées et les valeurs réelles
     fig, axes = plt.subplots(3, 1, figsize=(10, 10))
-    for ax, name in zip(
+    for ax, column_name, name in zip(
         axes,
-        ("contact length", "mean peak thickness", "mean valley thickness")
+        ("contact length", "mean peak thickness", "mean valley thickness"),
+        ("longueur de contact", "épaisseur moyenne aux pics", "épaisseur moyenne aux vallées")
     ):
-        plot_measures(ax, auto_measures[name], manu_measures[name], name)
+        plot_measures(ax, auto_measures[column_name], manu_measures[column_name], name)
     plt.show()
 
     # Effectue l'étalonnage de l'appareil de mesure
-    for name in ("contact length", "mean peak thickness", "mean valley thickness"):
-        auto = auto_measures[name]
-        manu = manu_measures[name]
-        etalonnage(auto, manu, name)
+    for column_name, name in zip(
+        ("contact length", "mean peak thickness", "mean valley thickness"),
+        ("longueur de contact", "épaisseur moyenne aux pics", "épaisseur moyenne aux vallées")
+    ):
+        etalonnage(auto_measures[column_name], manu_measures[column_name], name)
 
     plt.show()
